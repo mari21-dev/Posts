2b5a6c87e486b87c2c9041c4d4d153148e531c3f
bykovdy <bykovdy@it2g.ru>
2021-02-28 19:13:50 +0300

    01 - Рефакторинг

--- script.js
+++ script.js
@@ -1,31 +1,38 @@
 let postsArray = [];
 
 function createElement(tagName, className, html, id, ship) {
-  let element = document.createElement(tagName);
-  element.className = className;
-  element.innerHTML = html;
-  element.id = id;
-  ship.append(element);
+    const element = document.createElement(tagName);
+    element.className = className;
+    element.innerHTML = html;
+    element.id = id;
+    ship.append(element);
 }
 
-async function getPosts() {
-  let response = await fetch("https://jsonplaceholder.typicode.com/posts");
-  let posts = await response.json();
-  postsArray = posts;
-  postsArray.map((element) => {
-    const post = {
-      title: element.title,
-      body: element.body,
-    };
-
-    createElement(
-      "div",
-      "post",
-      `<h2>${post.title}</h2><p>${post.body}</p>`,
-      "",
-      document.querySelector(".container-post")
-    );
-  });
+async function fetchGetPosts() {
+    try {
+        let response = await fetch("https://jsonplaceholder.typicode.com/posts");
+        return await response.json();
+    } catch (e) {
+        alert(`Произошла ошибка в методе: ${e.message}`)
+    }
 }
 
-getPosts();
+const renderPosts = async () => {
+    postsArray = await fetchGetPosts();
+    postsArray.map((element) => {
+        const post = {
+            title: element.title,
+            body: element.body,
+        };
+
+        createElement(
+            "div",
+            "post",
+            `<h2>${post.title}</h2><p>${post.body}</p>`,
+            "",
+            document.querySelector(".container-post")
+        );
+    });
+}
+
+window.onload = () => renderPosts()
